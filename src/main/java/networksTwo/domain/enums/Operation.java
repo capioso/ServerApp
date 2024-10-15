8dd12569a1bf3cf99d251afaf17422f6a8decf43
Alexis Segales <Alexis.Segales@jala.university>
2024-10-14 22:52:43 -0400

    update

--- src/main/java/networksTwo/domain/enums/Operation.java
+++ src/main/java/networksTwo/domain/enums/Operation.java
@@ -3,13 +3,9 @@ package networksTwo.domain.enums;
 public enum Operation {
     CREATE_USER,
     GET_USER,
-    UPDATE_USER,
-    DELETE_USER,
     CREATE_CHAT,
     GET_CHATS,
     GET_SINGLE_CHAT,
-    UPDATE_CHAT,
-    DELETE_CHAT,
     SEND_MESSAGE,
     GET_MESSAGES_BY_CHAT,
     LOGIN_USER,
